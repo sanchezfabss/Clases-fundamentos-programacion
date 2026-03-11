d282618e96758c00bef430a985ea89036f2fce49
Karla Fabiola Perez Sanchez <karla.perez@lab-b8-15.cetav.local>
2026-03-11 13:58:01 -0600

    tarea/

---
+++ tarea/tarea.js
@@ -0,0 +1,9 @@
+/*Ejercicio 1*/ 
+const number = 50;
+const result = (number >= 10 && number <=20) || (number >= 40 && number <= 60);
+console.log("El calculo de rangos para el numero", number, "es ", result)
+
+/*Ejercicio 2*/
+const dogAge = 4
+const humanAge = 20 + (dogAge - 2) * 4;
+console.log("El perrito de ", dogAge, "años tendria ", humanAge, "si fuese humano")
